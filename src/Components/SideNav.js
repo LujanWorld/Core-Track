c2968ef30891df0d47f8134fa887831ff7418ccd
jossAcosta <josselineacostamtz@gmail.com>
2020-05-14 13:48:04 -0500

    :lipstick: styles in sidebar and views

--- src/Components/SideNav.js
+++ src/Components/SideNav.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import './Styles/SideNav.css';
+
 
 function SideNav() {
   return (
       <div className="sideNavBar">
-         
-         SideNav
+         <div className='sideNav_logo nav_grid1'>
+             <p>CORE TRACK</p>
+             <i className="fas fa-user-clock"></i>
+         </div>
+         <div className='sideNav_icono nav_grid2'>
+         <i className="fas fa-home"></i>
+         </div>
+         <div className='sideNav_icono nav_grid3'>
+         <i className="fas fa-users"></i>
+         </div>
+         <div className='sideNav_icono nav_grid4'>
+         <i className="fas fa-chart-pie"></i><p>USUARIO</p>
+         </div>
+         <div className='sideNav_icono nav_grid5'>
+         <i className="fas fa-chart-pie"></i><p>PROYECTO</p>
+         </div>
       </div>
    
   );
