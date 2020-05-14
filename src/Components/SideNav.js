de04e42e79113edf356bb301c4abc11c6877ae11
jossAcosta <josselineacostamtz@gmail.com>
2020-05-14 13:59:05 -0500

    added link to sidenav

--- src/Components/SideNav.js
+++ src/Components/SideNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import './Styles/SideNav.css';
 
@@ -8,19 +9,30 @@ function SideNav() {
       <div className="sideNavBar">
          <div className='sideNav_logo nav_grid1'>
              <p>CORE TRACK</p>
+            
              <i className="fas fa-user-clock"></i>
+            
+            
          </div>
          <div className='sideNav_icono nav_grid2'>
-         <i className="fas fa-home"></i>
+            <Link to="/dashboard">
+             <i className="fas fa-home"></i>
+            </Link>
          </div>
          <div className='sideNav_icono nav_grid3'>
+         <Link to="/usertask">
          <i className="fas fa-users"></i>
+         </Link>
          </div>
          <div className='sideNav_icono nav_grid4'>
+         <Link to="/userchart">
          <i className="fas fa-chart-pie"></i><p>USUARIO</p>
+         </Link>
          </div>
          <div className='sideNav_icono nav_grid5'>
+         <Link to="/projectchart">
          <i className="fas fa-chart-pie"></i><p>PROYECTO</p>
+         </Link>
          </div>
       </div>
    
