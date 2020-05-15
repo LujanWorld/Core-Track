7b4f3ba5454afe88c17367b96ca418e359d68b45
Mirey91 <mirey.morales91@gmail.com>
2020-05-15 00:08:06 -0500

    Regresar el dashboard

--- src/Views/Dashboard.js
+++ src/Views/Dashboard.js
@@ -1,28 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
 
-import projects from "./projects";
-import tasks from "./items";
 
 function Dashboard() {
-  const projectList = projects.map((project) => {
-    const tasksProject = tasks.filter((task) => task.rel_id === project.id);
-    return (
-      <div key={project.id}>
-        <Link to={`/projectchart/${project.id}`}><h3>{project.name}</h3></Link>
-        {tasksProject.map((task) => {
-          return <div>{task.name}</div>;
-        })}
-        <hr/>
-      </div>
-    );
-  });
-
   return (
     <div className="view_container">
-      <h2>All Projects</h2>
-      <hr />
-      {projectList}
+     Dashboard
     </div>
   );
 }
